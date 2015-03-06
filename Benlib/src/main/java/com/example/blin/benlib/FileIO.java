5f20bdbe0dd7138cf745e8fdf188fa7a03bb7135
bengal2009 <bennylin@outlook.com>
2015-03-06 17:32:03 +0800

    '1234'

--- Benlib/src/main/java/com/example/blin/benlib/FileIO.java
+++ Benlib/src/main/java/com/example/blin/benlib/FileIO.java
@@ -6,7 +6,10 @@ import android.util.Log;
  * Created by blin on 2015/3/6.
  */
     public class FileIO {
+
+private  String TAG="FileIO";
         public void Test() {
-            Log.i("Benlib", "1234");
+            Log.i(TAG, "1234");
+            Log.i(TAG, "1234");
         }
     }
