e9f4c2f055541c7ddf5b8147a2dc4f5c0f46b518
bengal2009 <bennylin@outlook.com>
2015-03-06 15:50:23 +0800

    Initial commit

---
+++ Benlib/src/main/java/com/example/blin/benlib/FileIO.java
@@ -0,0 +1,12 @@
+package com.example.blin.benlib;
+
+import android.util.Log;
+
+/**
+ * Created by blin on 2015/3/6.
+ */
+    public class FileIO {
+        public void Test() {
+            Log.i("Benlib", "1234");
+        }
+    }
