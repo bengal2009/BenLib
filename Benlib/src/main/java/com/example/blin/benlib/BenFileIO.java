f7067c0febad006c1fa00c33cec405617328679a
bengal2009 <bennylin@outlook.com>
2015-03-11 16:43:56 +0800

    'office'

--- Benlib/src/main/java/com/example/blin/benlib/BenFileIO.java
+++ Benlib/src/main/java/com/example/blin/benlib/BenFileIO.java
@@ -1,7 +1,13 @@
 package com.example.blin.benlib;
 
+import android.content.Context;
+import android.net.Uri;
+import android.os.Environment;
+import android.util.Log;
+
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
@@ -13,6 +19,60 @@ import java.util.ArrayList;
     public class BenFileIO {
 
 private  String TAG="FileIO";
+
+    /*Operating mode. Use 0 or MODE_PRIVATE for the default operation, MODE_APPEND to append to an existing file,
+    MODE_WORLD_READABLE and MODE_WORLD_WRITEABLE to control permissions.
+*/
+    public void FileWrite(Context mcontext,String filename,String string)
+    {
+
+        FileOutputStream outputStream;
+
+        try {
+            outputStream = mcontext.openFileOutput(filename, Context.MODE_PRIVATE);
+            outputStream.write(string.getBytes());
+            outputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public File getTempFile(Context context, String url) {
+        File file=null;
+        try {
+            String fileName = Uri.parse(url).getLastPathSegment();
+            file = File.createTempFile(fileName, null, context.getCacheDir());
+        }
+            catch (IOException E) {
+                // Error while creating file
+            }
+            return file;
+        }
+    /* Checks if external storage is available for read and write */
+    public boolean isExternalStorageWritable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state)) {
+            return true;
+        }
+        return false;
+    }
+    public File getAlbumStorageDir(String albumName) {
+        // Get the directory for the user's public pictures directory.
+        File file = new File(Environment.getExternalStoragePublicDirectory(
+                Environment.DIRECTORY_PICTURES), albumName);
+        if (!file.mkdirs()) {
+            Log.i(TAG, "Directory not created");
+        }
+        return file;
+    }
+    /* Checks if external storage is available to at least read */
+    public boolean isExternalStorageReadable() {
+        String state = Environment.getExternalStorageState();
+        if (Environment.MEDIA_MOUNTED.equals(state) ||
+                Environment.MEDIA_MOUNTED_READ_ONLY.equals(state)) {
+            return true;
+        }
+        return false;
+    }
 
     public ArrayList<File> getDirList(String DirPath){
         File root;
