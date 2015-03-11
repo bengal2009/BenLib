37b7e8d86a034da0ecb85acb61c6554d372abe09
bengal2009 <bennylin@outlook.com>
2015-03-11 11:16:10 +0800

    'office'

--- Benlib/src/main/java/com/example/blin/benlib/BenUtil.java
+++ Benlib/src/main/java/com/example/blin/benlib/BenUtil.java
@@ -2,8 +2,12 @@ package com.example.blin.benlib;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.provider.Settings;
 import android.util.Log;
 import android.view.Gravity;
@@ -127,6 +131,58 @@ LayoutInflater inflater = getLayoutInflater();
         mTextSwitcher.setOutAnimation(mcontext ,android.R.anim.fade_out);
     }
 
+    //添加通知到顶部任务栏
+    public void AddNotification(Context mcontext,int ID){
+        //添加通知到顶部任务栏
+        //获得NotificationManager实例
+        String service = mcontext.NOTIFICATION_SERVICE;
+        NotificationManager nm = (NotificationManager) mcontext.getSystemService(service);
+        //实例化Notification
+        Notification n = new Notification();
+        //设置显示图标
+        //设置提示信息
+        String tickerText ="我的程序";
+        //显示时间
+        long when = System.currentTimeMillis();
+
+        n.tickerText = tickerText;
+        n.when = when;
+        //显示在“正在进行中”
+        //  n.flags = Notification.FLAG_ONGOING_EVENT;
+        n.flags|=Notification.FLAG_AUTO_CANCEL; //自动终止
+        //实例化Intent
+//        Intent it = new Intent(mcontext,mcontext.getClass());
+        Intent it = new Intent(mcontext,mcontext.getClass());
+//        it.putExtra(KEY_COUNT, count);
+        /*********************
+         *获得PendingIntent
+         *FLAG_CANCEL_CURRENT:
+         *		如果当前系统中已经存在一个相同的PendingIntent对象，
+         *		那么就将先将已有的PendingIntent取消，然后重新生成一个PendingIntent对象。
+         *FLAG_NO_CREATE:
+         *		如果当前系统中不存在相同的PendingIntent对象，
+         *		系统将不会创建该PendingIntent对象而是直接返回null。
+         *FLAG_ONE_SHOT:
+         *		该PendingIntent只作用一次，
+         *		如果该PendingIntent对象已经触发过一次，
+         *		那么下次再获取该PendingIntent并且再触发时，
+         *		系统将会返回一个SendIntentException，在使用这个标志的时候一定要注意哦。
+         *FLAG_UPDATE_CURRENT:
+         *		如果系统中已存在该PendingIntent对象，
+         *		那么系统将保留该PendingIntent对象，
+         *		但是会使用新的Intent来更新之前PendingIntent中的Intent对象数据，
+         *		例如更新Intent中的Extras。这个非常有用，
+         *		例如之前提到的，我们需要在每次更新之后更新Intent中的Extras数据，
+         *		达到在不同时机传递给MainActivity不同的参数，实现不同的效果。
+         *********************/
+
+        PendingIntent pi = PendingIntent.getActivity(mcontext, 0, it, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        //设置事件信息，显示在拉开的里面
+        n.setLatestEventInfo(mcontext,"我的软件", "我的软件正在运行……", pi);
+        //发出通知
+        nm.notify(ID,n);
+    }
 
     //getting unique id for device
     public String SystetemID(Context mcontext) {
